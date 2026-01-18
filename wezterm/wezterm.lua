0a337ee4e0d7a41e63f58d6de316a016eb61cc72
Samemaru07 <osame0830@icloud.com>
2026-01-18 19:43:43 +0900

    feat: タブ切り替えのキーバインドを設定

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -237,7 +237,54 @@ config.keys = {
         key = "0",
         mods = "CTRL",
         action = wezterm.action.ResetFontSize
-    }
+    },
+
+    -- タブ切り替え
+    {
+        key = "1",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(0)
+    },
+    {
+        key = "2",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(1)
+    },
+    {
+        key = "3",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(2)
+    },
+    {
+        key = "4",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(3)
+    },
+    {
+        key = "5",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(4)
+    },
+    {
+        key = "6",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(5)
+    },
+    {
+        key = "7",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(6)
+    },
+    {
+        key = "8",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(7)
+    },
+    {
+        key = "9",
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(-1)
+    },
 }
 
 config.key_tables = {
